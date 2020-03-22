8adbdaabec1da9d5f4e2dfb0ba90e4cd763f1484
titov svyatoslav <slava.titoff@yandex.ru>
2020-03-22 11:11:32 +0400

    Initial Commit

---
+++ reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reminder
+//
+//  Created by Svyatoslav Titov on 22.03.2020.
+//  Copyright © 2020 Svyatoslav Titov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
